70fb42bbc91a5ecf76676bbdf363fccf9d4bf08d
maerdlina <maerdlina@gmail.com>
2025-05-01 13:42:15 +0300

    add 1

---
+++ ArrayListVsLinkedList/src/ExampleSecond.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ExampleSecond {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] list = new String[10];
+
+        for(int i = 0; i < list.length; i++){
+            list[i] = reader.readLine();
+        }
+    }
+}
