a99fa80145919152702156e2845fa9d367ce0f09
maerdlina <maerdlina@gmail.com>
2025-05-07 22:08:17 +0300

    JAVA-11/Miro-Project-To-Do-Done

---
+++ Micro Prjects/ToDoArrayList/src/main/java/org/anki/App.java
@@ -0,0 +1,44 @@
+package org.anki;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class App
+{
+    public static void main( String[] args ) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        Boolean createNotes = true;
+        ArrayList<Note> notesList = new ArrayList<>();
+
+        while(createNotes){
+            System.out.println("Вы хотите создать заметку? Введите: true или false");
+            createNotes = Boolean.parseBoolean(read.readLine().toLowerCase(Locale.ROOT));
+            if(createNotes == true) {
+                System.out.println("Введите сначала название, потом текст");
+                Note note = new Note(read.readLine(), read.readLine());
+                notesList.add(note);
+            } else break;
+
+            Boolean statusBool = true;
+            while(statusBool){
+                System.out.println("Вы хотите поменять статус задачи по индексу?");
+                statusBool = Boolean.parseBoolean(read.readLine().toLowerCase(Locale.ROOT));
+                if(statusBool == false) break;
+                else{
+                    System.out.println("Введите индекс");
+                    Integer index = Integer.parseInt(read.readLine()) - 1;
+                    System.out.println("Введите значение статуса: true - done, false - not done:");
+                    notesList.get(index).changeStatus(Boolean.parseBoolean(read.readLine().toLowerCase(Locale.ROOT)));
+                }
+
+            }
+
+
+
+        }
+        for(Note s: notesList) System.out.println(s.toString());
+    }
+}
