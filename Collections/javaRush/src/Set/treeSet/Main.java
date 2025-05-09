98b414fccb16703a20ee7c83cde5d56f1555c70c
maerdlina <maerdlina@gmail.com>
2025-05-09 20:01:58 +0300

    JAVA-60 <message>

--- Collections/javaRush/src/Set/treeSet/Main.java
+++ Collections/javaRush/src/Set/treeSet/Main.java
@@ -1,4 +1,13 @@
 package Set.treeSet;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
+    public static void main(String[] args){
+        Set<String> my = new TreeSet<>();
+        my.add("banana");
+        my.add("hewllo");
+        System.out.println(my);
+    }
 }
