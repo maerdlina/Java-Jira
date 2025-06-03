8e16d6e4a9e6f3ba9784bbea9edf5ed282408f8b
maerdlina <maerdlina@gmail.com>
2025-06-03 18:01:02 +0300

    done
    Servlet work in http://localhost:8085/tomcatTest-0.0.1-SNAPSHOT/hello

---
+++ Backend/tomcatTest/src/main/java/com/backend/tomcattest/servlet/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.backend.tomcattest.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        try{
+            writer.println("<h2>Hello from HelloServlet</h2>");
+        } finally {
+            writer.close();
+        }
+    }
+}
